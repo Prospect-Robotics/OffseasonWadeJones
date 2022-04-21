f9f592c90c1486e1da0d01ba555c30e66beaa54b
abhineetpal <aip_embox@thepal.org>
2022-04-20 17:29:32 -0700

    creating wrapper for spark max

---
+++ OffseasonWadeJones/src/main/java/com/team2813/lib/motors/SparkMaxWrapper.java
@@ -0,0 +1,50 @@
+package com.team2813.lib.motors;
+
+import com.revrobotics.CANSparkMax;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SparkMaxWrapper extends CANSparkMax {
+    List<CANSparkMax> followers = new ArrayList<>();
+
+    /**
+     * Create a new object to control a SPARK MAX motor Controller
+     *
+     * @param deviceId The device ID.
+     * @param type     The motor type connected to the controller. Brushless motor wires must be connected
+     *                 to their matching colors and the hall sensor must be plugged in. Brushed motors must be
+     */
+    public SparkMaxWrapper(int deviceId, MotorType type) {
+        super(deviceId, type);
+
+        restoreFactoryDefaults();
+
+        enableVoltageCompensation(12);
+        setClosedLoopRampRate(0);
+        setPeriodicFramePeriod(PeriodicFrame.kStatus2, 20);
+        setSmartCurrentLimit(40);
+    }
+
+    /**
+     * Sets the PID coefficients for the PID at the given slot id
+     * @param slotID Is the gain schedule slot, the value is a number between 0 and 3. Each slot has
+     *     its own set of gain values and can be changed in each control frame using SetReference().
+     */
+    public void configPIDF(double p, double i, double d, double f, int slotID) {
+        getPIDController().setP(p, slotID);
+        getPIDController().setI(i, slotID);
+        getPIDController().setD(d, slotID);
+        getPIDController().setFF(f, slotID);
+    }
+
+    public void configPIDF(double p, double i, double d, double f) {
+        configPIDF(p, i, d, f, 0);
+    }
+
+    public void addFollower(int deviceNumber, boolean inverted) {
+        CANSparkMax follower = new CANSparkMax(deviceNumber, MotorType.kBrushless);
+        follower.follow(this, inverted);
+        followers.add(follower); // add to the list to preserve CANSparkMax follower object
+    }
+}
--- OffseasonWadeJones/src/main/java/com/team2813/lib/motors/TalonFXWrapper.java
+++ OffseasonWadeJones/src/main/java/com/team2813/lib/motors/TalonFXWrapper.java
@@ -11,26 +11,42 @@ import java.util.List;
 public class TalonFXWrapper extends TalonFX {
     List<TalonFX> followers = new ArrayList<>();
 
+    /**
+     * Constructor
+     * @param deviceNumber [0,62]
+     * @param canbus Name of the CANbus; can be a SocketCAN interface (on Linux),
+     *               or a CANivore device name or serial number
+     */
     public TalonFXWrapper(int deviceNumber, String canbus) {
         super(deviceNumber, canbus);
 
         configAllSettings(new TalonFXConfiguration());
 
         enableVoltageCompensation(true);
+        configClosedloopRamp(0);
         configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 40, 40, 0.25));
         configVoltageCompSaturation(12);
     }
 
+    /**
+     * Constructor
+     * @param deviceNumber [0,62]
+     */
     public TalonFXWrapper(int deviceNumber) {
         super(deviceNumber);
 
         configAllSettings(new TalonFXConfiguration());
 
         enableVoltageCompensation(true);
+        configClosedloopRamp(0);
         configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 40, 40, 0.25));
         configVoltageCompSaturation(12);
     }
 
+    /**
+     * Sets the PID coefficients for the PID at the given slot id (0 for Primary PID, 1 for Auxiliary PID)
+     * @param slotID Parameter slot for the constant
+     */
     public void configPIDF(double p, double i, double d, double f, int slotID) {
         config_kP(slotID, p);
         config_kI(slotID, i);
@@ -42,13 +58,13 @@ public class TalonFXWrapper extends TalonFX {
         TalonFX follower = new TalonFX(deviceNumber, canbus);
         follower.follow(this);
         follower.setInverted(invertType);
-        followers.add(follower);
+        followers.add(follower); // add to the list to preserve TalonFX follower object
     }
 
     public void addFollower(int deviceNumber, TalonFXInvertType invertType) {
         TalonFX follower = new TalonFX(deviceNumber);
         follower.follow(this);
         follower.setInverted(invertType);
-        followers.add(follower);
+        followers.add(follower); // add to the list to preserve TalonFX follower object
     }
 }
