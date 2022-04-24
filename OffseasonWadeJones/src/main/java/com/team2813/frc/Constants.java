caf51e5e6a8ca2cd1a33f12df401ac5c64190e41
abhineetpal <aip_embox@thepal.org>
2022-04-23 22:17:03 -0700

    adding support for west coast, part 2
    Forgot some stuff

--- OffseasonWadeJones/src/main/java/com/team2813/frc/Constants.java
+++ OffseasonWadeJones/src/main/java/com/team2813/frc/Constants.java
@@ -5,6 +5,8 @@
 
 package com.team2813.frc;
 
+import edu.wpi.first.math.util.Units;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -13,4 +15,9 @@ package com.team2813.frc;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    // drive
+    public static final double WHEEL_DIAMETER = Units.inchesToMeters(4);
+    public static final double WHEEL_CIRCUMFERENCE = Math.PI * WHEEL_DIAMETER;
+    public static final double GEAR_RATIO = 1 / 7.64;
+}
--- OffseasonWadeJones/src/main/java/com/team2813/frc/util/Units2813.java
+++ OffseasonWadeJones/src/main/java/com/team2813/frc/util/Units2813.java
@@ -1,5 +1,10 @@
 package com.team2813.frc.util;
 
+import com.team2813.lib.drive.DriveDemand;
+
+import static com.team2813.frc.Constants.GEAR_RATIO;
+import static com.team2813.frc.Constants.WHEEL_CIRCUMFERENCE;
+
 public class Units2813 {
     public static double ticksToMotorRevs(double ticks, int cpr) {
         return ticks / cpr;
@@ -15,5 +20,18 @@ public class Units2813 {
 
     public static double wheelRevsToMotorRevs(double revs, double gearRatio) {
         return revs / gearRatio;
+    }
+
+    // drivetrain velocity to motor rpms
+    public static double dtVelocityToMotorRpm(double speed) {
+        return wheelRevsToMotorRevs(speed / WHEEL_CIRCUMFERENCE, GEAR_RATIO) * 60;
+    }
+
+    public static double motorRpmToDtVelocity(double rpm) {
+        return motorRevsToWheelRevs(rpm, GEAR_RATIO) * WHEEL_CIRCUMFERENCE / 60;
+    }
+
+    public static DriveDemand dtDemandToMotorDemand(DriveDemand demand) { // input in m/s output in motor rpm
+        return new DriveDemand(dtVelocityToMotorRpm(demand.getLeft()), dtVelocityToMotorRpm(demand.getRight()));
     }
 }
