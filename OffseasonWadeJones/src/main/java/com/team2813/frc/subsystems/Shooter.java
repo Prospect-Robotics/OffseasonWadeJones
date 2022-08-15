b1ecc69f8b68e71219c24e2ad3f39620d91d120a
abhineetpal <aip_embox@thepal.org>
2022-08-14 18:39:52 -0700

    shooter subsystem cleanup

--- OffseasonWadeJones/src/main/java/com/team2813/frc/subsystems/Shooter.java
+++ OffseasonWadeJones/src/main/java/com/team2813/frc/subsystems/Shooter.java
@@ -1,42 +1,38 @@
 package com.team2813.frc.subsystems;
+
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
+import com.team2813.lib.motors.ControlMode;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.math.util.Units;
-import com.ctre.phoenix.motorcontrol.StatusFrame;
 import com.team2813.lib.motors.TalonFXWrapper;
 import com.team2813.frc.util.Limelight;
 import com.team2813.frc.util.Units2813;
-import com.team2813.frc.Constants.ShooterConstants;
-import com.team2813.lib.motors.interfaces.ControlMode;
 
-
-import com.team2813.lib.controls.Controller;
-import com.team2813.lib.controls.Axis;
-import com.team2813.lib.controls.Button;
+import static com.team2813.frc.Constants.*;
 
 public class Shooter extends SubsystemBase 
 {
-    private final TalonFXWrapper FLYWHEEL;
+    private final double kP = 0.75;
+    private final double kI = 0.0025;
+    private final double kD = 2.5;
 
-    private Limelight limelight = Limelight.getInstance();
+    private final TalonFXWrapper flywheel = new TalonFXWrapper(7, TalonFXInvertType.Clockwise);
+    private final SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(1.2795, 0.18545, 0.081586);
+    private final Limelight limelight = Limelight.getInstance();
 
-    private double demand = 0;
-    //private final double spoolDemand = 0.435;
-
-    private static final Button SHOOTER_BUTTON = ShooterConstants.getShooterButton();
-    private static final Button MANUAL_SHOOT_BUTTON = ShooterConstants.getManualShootButton();
-    private static final Button LOW_SHOOT_BUTTON = ShooterConstants.getLowShootButton();
-    private static final Button SPOOL_BUTTON = ShooterConstants.getSpoolButton();
-
+    private double demand;
     private boolean isFullyRevvedUp;
 
     public Shooter() {
-        FLYWHEEL = (TalonFXWrapper) MotorConfigs.talons.get("flywheel");
-        FLYWHEEL.setStatusFramePeriod(StatusFrame.Status_13_Base_PIDF0, 200);
+        flywheel.addFollower(8, TalonFXInvertType.OpposeMaster);
+        flywheel.setStatusFramePeriod(StatusFrameEnhanced.Status_21_FeedbackIntegrated, 125);
+        flywheel.configPID(kP, kI, kD);
     }
 
     public boolean isFlywheelReady() {
-        return Math.abs(Units2813.motorRevsToWheelRevs(FLYWHEEL.getVelocity(), ShooterConstants.FLYWHEEL_UPDUCTION) - demand) < 175;
+        return Math.abs(Units2813.motorRevsToWheelRevs(flywheel.getVelocity(), FLYWHEEL_UPDUCTION) - demand) < 25;
     }
 
     boolean isFullyRevvedUp() {
@@ -44,97 +40,18 @@ public class Shooter extends SubsystemBase
     }
 
     @Override
-    public void outputTelemetry() {
-        double flywheelVelocity = Units2813.motorRevsToWheelRevs(FLYWHEEL.getVelocity(), ShooterConstants.FLYWHEEL_UPDUCTION);
+    public void periodic() {
+        double flywheelVelocity = Units2813.motorRevsToWheelRevs(flywheel.getVelocity(), FLYWHEEL_UPDUCTION);
         double error = demand - flywheelVelocity;
         SmartDashboard.putNumber("Flywheel Demand", demand);
         SmartDashboard.putNumber("Flywheel Velocity", flywheelVelocity);
         SmartDashboard.putNumber("Flywheel Error", error);
-        SmartDashboard.putNumber("Flywheel Encoder", FLYWHEEL.getEncoderPosition());
+        SmartDashboard.putNumber("Flywheel Encoder", flywheel.getEncoderPosition());
         SmartDashboard.putNumber("Distance to Target", limelight.calculateHorizontalDistance());
     }
 
-    public void teleopControls() {
-        SPOOL_BUTTON.whenPressed(() -> setShooter(ShooterConstants.spoolDemand));
-
-        if (SHOOTER_BUTTON.get()) {
-            if (DRIVE.getIsAimed()) {
-                setShooter(limelight.getShooterDemand());
-                if (isFlywheelReady()) {
-                    MAGAZINE.setMagDemand(Magazine.MagDemand.SHOOT);
-                    MAGAZINE.setKickerDemand(Magazine.KickerDemand.IN);
-                }
-                else {
-                    MAGAZINE.setMagDemand(Magazine.MagDemand.OFF);
-                    MAGAZINE.setKickerDemand(Magazine.KickerDemand.OFF);
-                }
-            }
-        }
-
-        SHOOTER_BUTTON.whenReleased(() -> {
-            setShooter(0);
-            MAGAZINE.setMagDemand(Magazine.MagDemand.OFF);
-            MAGAZINE.setKickerDemand(Magazine.KickerDemand.OFF);
-        });
-
-        if (MANUAL_SHOOT_BUTTON.get()) {
-            //setShooter(limelight.getShooterDemand());
-            if (isFlywheelReady()) {
-                MAGAZINE.setMagDemand(Magazine.MagDemand.SHOOT);
-                MAGAZINE.setKickerDemand(Magazine.KickerDemand.IN);
-            }
-            else {
-                MAGAZINE.setMagDemand(Magazine.MagDemand.OFF);
-                MAGAZINE.setKickerDemand(Magazine.KickerDemand.OFF);
-            }
-        }
-
-        MANUAL_SHOOT_BUTTON.whenReleased(() -> {
-            setShooter(0);
-            MAGAZINE.setMagDemand(Magazine.MagDemand.OFF);
-            MAGAZINE.setKickerDemand(Magazine.KickerDemand.OFF);
-        });
-
-        if (LOW_SHOOT_BUTTON.get()) {
-            setShooter(ShooterConstants.lowDemand);
-            if (isFlywheelReady()) {
-                MAGAZINE.setMagDemand(Magazine.MagDemand.SHOOT);
-                MAGAZINE.setKickerDemand(Magazine.KickerDemand.LOW);
-            }
-            else {
-                MAGAZINE.setMagDemand(Magazine.MagDemand.OFF);
-                MAGAZINE.setKickerDemand(Magazine.KickerDemand.OFF);
-            }
-        }
-
-        LOW_SHOOT_BUTTON.whenReleased(() -> {
-            setShooter(0);
-            MAGAZINE.setMagDemand(Magazine.MagDemand.OFF);
-            MAGAZINE.setKickerDemand(Magazine.KickerDemand.OFF);
-        });
-
-        isFullyRevvedUp = FLYWHEEL.getVelocity() >= Units2813.wheelRevsToMotorRevs(demand, FLYWHEEL_UPDUCTION);
-    }
-
-    @Override
-    protected void writePeriodicOutputs() {
-        if (demand == 0) {
-            double error = Math.abs(demand - Units2813.motorRevsToWheelRevs(FLYWHEEL.getVelocity(), FLYWHEEL_UPDUCTION));
-            if (error <= 10) {
-                FLYWHEEL.set(ControlMode.DUTY_CYCLE, 0);
-            }
-            else {
-                FLYWHEEL.set(ControlMode.VELOCITY, 0);
-            }
-        }
-        else {
-            double motorDemand = Units2813.wheelRevsToMotorRevs(demand, FLYWHEEL_UPDUCTION);
-            FLYWHEEL.set(ControlMode.VELOCITY, motorDemand);
-        }
-    }
-
     public void setShooter(double demand) {
-        this.demand = demand;
+        double motorDemand = Units2813.wheelRevsToMotorRevs(demand, FLYWHEEL_UPDUCTION);
+        flywheel.set(ControlMode.VELOCITY, motorDemand, feedforward.calculate(motorDemand / 60));
     }
-
 }
