75c2d096a24d979ed0c72b5462e4425793ef963e
abhineetpal <aip_embox@thepal.org>
2022-09-10 18:40:41 -0700

    fixed errors
    There were errors caused by missing or extra commas. Also, the 3-ball paths need to be modified because they have awkward angles that the robot might not be able to do.

--- OffseasonWadeJones/src/main/java/com/team2813/frc/AutoRoutine.java
+++ OffseasonWadeJones/src/main/java/com/team2813/frc/AutoRoutine.java
@@ -2,6 +2,7 @@ package com.team2813.frc;
 
 import com.team2813.frc.commands.*;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 
@@ -31,30 +32,26 @@ public enum AutoRoutine {
             new AutoShootCommand(ROBOT_CONTAINER.getDrive(), ROBOT_CONTAINER.getMagazine(), ROBOT_CONTAINER.getShooter())
     )),
     THREE_BALL_BASIC("3-ball Basic", new SequentialCommandGroup(
-            new InstantCommand(() -> ROBOT_CONTAINER.getShooter().setFlywheelRPM(DEFAULT_SHOOT_DEMAND)),
             new AutoInitDriveCommand("ThreeBall_Shoot", true, ROBOT_CONTAINER.getDrive()),
             new FollowCommand("ThreeBall_Shoot", ROBOT_CONTAINER.getDrive()),
             new AutoShootCommand(ROBOT_CONTAINER.getDrive(), ROBOT_CONTAINER.getMagazine(), ROBOT_CONTAINER.getShooter()),
             new AutoIntakeCommand(ROBOT_CONTAINER.getIntake(), ROBOT_CONTAINER.getMagazine()),
-            //new WaitCommand(1),
             new FollowCommand("ThreeBall_Shoot", true, ROBOT_CONTAINER.getDrive()),
             new AutoStopIntakeCommand(ROBOT_CONTAINER.getIntake(), ROBOT_CONTAINER.getMagazine()),
             new FollowCommand("ThreeBall_Shoot", true, ROBOT_CONTAINER.getDrive()),
             new AutoShootCommand(ROBOT_CONTAINER.getDrive(), ROBOT_CONTAINER.getMagazine(), ROBOT_CONTAINER.getShooter())
-    ))
+    )),
     THREE_BALL_KNOCK("3-ball Knock", new SequentialCommandGroup(
-        new InstantCommand(() -> ROBOT_CONTAINER.getShooter().setFlywheelRPM(DEFAULT_SHOOT_DEMAND)),
         new FollowCommand("ThreeBall_Shoot", ROBOT_CONTAINER.getDrive()),
         new AutoShootCommand(ROBOT_CONTAINER.getDrive(), ROBOT_CONTAINER.getMagazine(), ROBOT_CONTAINER.getShooter()),
         new FollowCommand("ThreeBall_Shoot", ROBOT_CONTAINER.getDrive()),
         new AutoIntakeCommand(ROBOT_CONTAINER.getIntake(), ROBOT_CONTAINER.getMagazine()),
-        //new WaitCommand(1),
+        new WaitCommand(1),
         new AutoStopIntakeCommand(ROBOT_CONTAINER.getIntake(), ROBOT_CONTAINER.getMagazine()),
         new FollowCommand("ThreeBall_Shoot", ROBOT_CONTAINER.getDrive()),
         new AutoShootCommand(ROBOT_CONTAINER.getDrive(), ROBOT_CONTAINER.getMagazine(), ROBOT_CONTAINER.getShooter())
     )),
     THREE_BALL_COMPLEX("3-ball Complex", new SequentialCommandGroup(
-        new InstantCommand(() -> ROBOT_CONTAINER.getShooter().setFlywheelRPM(DEFAULT_SHOOT_DEMAND)),
         new FollowCommand("ThreeBall_Shoot", ROBOT_CONTAINER.getDrive()),
         new AutoShootCommand(ROBOT_CONTAINER.getDrive(), ROBOT_CONTAINER.getMagazine(), ROBOT_CONTAINER.getShooter()),
         new AutoShootCommand(ROBOT_CONTAINER.getDrive(), ROBOT_CONTAINER.getMagazine(), ROBOT_CONTAINER.getShooter()),
@@ -67,7 +64,7 @@ public enum AutoRoutine {
         new FollowCommand("ThreeBall_Shoot", ROBOT_CONTAINER.getDrive()),
         new AutoIntakeCommand(ROBOT_CONTAINER.getIntake(), ROBOT_CONTAINER.getMagazine()),
         new WaitCommand(1),
-        new AutoStopIntakeCommand(ROBOT_CONTAINER.getIntake(), ROBOT_CONTAINER.getMagazine()),
+        new AutoStopIntakeCommand(ROBOT_CONTAINER.getIntake(), ROBOT_CONTAINER.getMagazine())
     )),
     ROTATE_90_TEST("Rotate 90 Test", new RotateCommand(90, ROBOT_CONTAINER.getDrive())),
     ROTATE_180_TEST("Rotate 180 Test", new RotateCommand(180, ROBOT_CONTAINER.getDrive())),
